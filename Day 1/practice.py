be614d46b7dffa04695b9c12df2f451dfd38323b
Mebz Moren <ellyzajpapas@su.edu.ph>
2023-02-22 00:54:40 +0800

    debugging exercise

--- Day 1/practice.py
+++ Day 1/practice.py
@@ -1,3 +1,10 @@
 # Write your code below this line
-print("Hello world!") #prints hello world on the terminal/console, these are known as strings
-print("hi")
+print("Hello world!")       #prints hello world on the terminal/console, these are known as strings
+
+print('print("hi")hi')      #if you want to print double quotes
+print('hi')                 #single quotes can be used to determine a string instead of a double quote
+
+print("hello\nworld")       #can print out 2 lines of strings for one line of code
+print("hello\nworld\n")     #can add a new blank line to print
+print("hello" + "mebz")     #combines 2 strings, no space in between
+print("hello " + "mebz")    #combines 2 strings
