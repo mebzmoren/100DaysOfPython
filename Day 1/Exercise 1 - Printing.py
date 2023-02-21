f5b45ccc95d6030a3d5dd61576ba13a7b2b55794
Mebz Moren <ellyzajpapas@su.edu.ph>
2023-02-22 00:30:31 +0800

    printing exercise

---
+++ Day 1/Exercise 1 - Printing.py
@@ -0,0 +1,4 @@
+#Write a program that prints
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
--- Day 1/practice.py
+++ Day 1/practice.py
@@ -1,2 +1,3 @@
 # Write your code below this line
-print("Hello world!") #prints hello world on the terminal/shell, these are known as strings
+print("Hello world!") #prints hello world on the terminal/console, these are known as strings
+print("hi")
