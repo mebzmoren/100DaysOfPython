faa3744cf33918abaed74c8202b75c1a9302014c
mebzmoren <ellyzajpapas@su.edu.ph>
2023-02-23 14:56:47 +0800

    pizza ordering exercise

--- Day 3/practice.py
+++ Day 3/practice.py
@@ -33,3 +33,23 @@ if height > 100:
 else:
     print("below 100")
 print("\n")
+
+#multiple if statements
+#elif condition
+if height > 100:
+    print("above 100")
+    if age < 12:
+        print("less than 12")
+    elif age <= 18:
+        print("below and equal to 18")
+    elif age == 19:
+        print ("is 19")
+    else:
+        print("above 18")
+
+    decicion = "Y"
+    if decicion == ("Y"or "y"):
+        print("\nyes")
+else:
+    print("below 100")
+print("\n")
