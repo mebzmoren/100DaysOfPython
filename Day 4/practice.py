1c16c66e9592128e3fd0bcd4dbf107bb0d6c538d
Mebz Moren <ellyzajpapas@su.edu.ph>
2023-02-24 13:13:46 +0800

    heads/tails exercise

---
+++ Day 4/practice.py
@@ -0,0 +1,9 @@
+#randomisation & modules
+import random
+import practiceModule
+
+a = 1
+b = 10
+print(random.randint(a, b))     #prints random integer
+print(practiceModule.pi)        #how a module is used
+print(random.random() * 5)          #prints a random float number
