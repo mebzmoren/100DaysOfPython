9146c15c7269a4693b2a4ae8227a0e13cdb90414
Mebz Moren <ellyzajpapas@su.edu.ph>
2023-02-24 13:57:01 +0800

    roulette exercise

--- Day 4/practice.py
+++ Day 4/practice.py
@@ -4,6 +4,22 @@ import practiceModule
 
 a = 1
 b = 10
+
+print("\n")
 print(random.randint(a, b))     #prints random integer
 print(practiceModule.pi)        #how a module is used
-print(random.random() * 5)          #prints a random float number
+print(random.random() * 5)      #prints a random float number
+print("\n")
+
+#lists
+list = ["ellyza", "eduardo", "dfjidf", "dfsdfadf"]
+print(list)                                         #prints the whole list
+print(list[1])                                      #prints an  iten in the list
+list[3] = "hi"                                      #changes an intem to the list
+print(list)
+list.append("hello")                                #adds an item to the end of the list
+list[3] = "hi"                                      #changes an intem to the list
+print(list)
+list.extend(["goodbye", "goodmorning"])             #extends the list
+print(list)
+print("\n")
