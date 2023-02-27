82355880bd74ecec52c2481968a8b45548c218a2
Mebz Moren <ellyzajpapas@su.edu.ph>
2023-02-27 22:51:38 +0800

    indents

--- Day 7/Challenge 4.py
+++ Day 7/Challenge 4.py
@@ -77,32 +77,31 @@ for _ in range(word_length):
     display += "_"
 
 while not end_of_game:
-guess = input("Guess a letter: ").lower()
+	guess = input("Guess a letter: ").lower()
 
     #Check guessed letter
-    for position in range(word_length):
-        letter = chosen_word[position]
+	for position in range(word_length):
+		letter = chosen_word[position]
         # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-        if letter == guess:
-            display[position] = letter
+		if letter == guess:
+			display[position] = letter
 
     #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
-    if guess not in chosen_word:
-    	lives = lives - 1
-
-    if lives == 0:
-        print("You loose.")
-        end_of_game = True
+	if guess not in chosen_word:
+		lives -= 1
+		if lives == 0:
+			end_of_game = True
+			print("You loose.")
 
     #Join all the elements in the list and turn it into a String.
-    print(f"{' '.join(display)}")
+	print(f"{' '.join(display)}")
 
     #Check if user has got all letters.
-    if "_" not in display:
-        end_of_game = True
-        print("You win.")
+	if "_" not in display:
+		end_of_game = True
+		print("You win.")
 
     #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
-    print(stages[lives])
+	print(stages[lives])
