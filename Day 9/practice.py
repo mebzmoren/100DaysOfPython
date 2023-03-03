a0eb593b6bc1e1f3f81db30304b1909e955148fc
Mebz Moren <ellyzajpapas@su.edu.ph>
2023-03-03 13:39:00 +0800

    dictionary in list exercise

--- Day 9/practice.py
+++ Day 9/practice.py
@@ -15,3 +15,31 @@ for key in programming_dictionary:
 newDictionary = {}                                                                      #empty dicionary
 programming_dictionary = {}                                                             #to wipe a dictionary
 print(programming_dictionary)
+print("\n")
+
+#nesting lists & dictionaries
+travels = {                                                                             #nesting a list in a dictionary
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+}
+print(travels)                                                                          #nesting a list in a list
+letters = ["a", "b", ["c", "d"]]
+print(letters)
+newTravels = {                                                                          #nesting a dictionary in a dictionary
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 1},
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 3}
+}
+print(newTravels)
+newTravels1 = [                                                                         #nesting a dictionary in a list
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visits": 1
+    },
+    {
+        "country":"Germany",
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 3
+    }
+]
+print(newTravels1)
