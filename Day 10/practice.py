a45a5310d33be1943085757e17d7a38e5468aa05
mebzmoren <ellyzajpapas@su.edu.ph>
2023-03-06 16:05:46 +0800

    calculator add subtract multiply divide functions

--- Day 10/practice.py
+++ Day 10/practice.py
@@ -7,6 +7,9 @@ print(something())                              #prints returned value
 print("hehe".title())                           #converts the 1st letter of the words into upper case
 
 def formating(fName, lName):
+    """Take a first and last name and format it 
+    to return the title case version of the
+    name"""                                     #docstring/multiline comment
     if fName == "" or lName =="":
         return                                  #terminates the function
     name = fName.title() + " " + lName.title()
