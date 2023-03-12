7cfd12d25cb071bb180c08af341d3feda0a5c90e
Mebz Moren <ellyzajpapas@su.edu.ph>
2023-03-12 17:35:03 +0800

    blackjack captsone

--- Day 11/blackjack.py
+++ Day 11/blackjack.py
@@ -6,9 +6,7 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 def rCards(list):
     while len(list) < 2:
         aCards(list)
-
     check = adding(list)
-
     if check > 20:
         for i in range(len(list)):
             if list[i] == 11:
@@ -24,7 +22,6 @@ def adding(list):
     i = 0
     for number in list:
         i += number
-
     return i
 
 def iPrint(list1, list2, sum1):
@@ -36,40 +33,40 @@ def fPrint(list1, list2, sum1, sum2):
     print(f"Computer's final hand: {list2}, final score: {sum2}")
 
 def check(list1, list2, sum1, sum2, prompt):
-    if sum1 == sum2:
+    if sum1 == sum2 or (prompt == "n" and sum1 == sum2):
         fPrint(list1, list2, sum1, sum2)
         print("Draw!")
     elif sum1 > 21 or (prompt == "n" and sum1 < sum2):
         fPrint(list1, list2, sum1, sum2)
         print("You loose!")
-    elif sum1 == 21:
+    elif sum1 == 21 or (prompt == "n" and sum1 > sum2 and sum1 < 22):
         fPrint(list1, list2, sum1, sum2)
         print("You win!")
 
-
 def game():
-
     print(logo)
     uCard = []
     cCard = []
-        
+    over = False
     rCards(uCard)
     rCards(cCard)
     sum_uCard = adding(uCard)
     sum_cCard = adding(cCard)
+    iPrint(uCard, cCard, sum_uCard)
 
-    while not :
-        iPrint(uCard, cCard, sum_uCard)
-    
+    while not over:
         prompt = input("Type 'y' to get another card, type 'n' to pass: ")
 
-            while prompt == "y":
+        if prompt  == "y":
             cards[0] = 1
             aCards(uCard)
             sum_uCard = adding(uCard)
             iPrint(uCard, cCard, sum_uCard)
-        
-        
-    check(uCard, cCard, sum_uCard, sum_cCard, prompt)
+            check(uCard, cCard, sum_uCard, sum_cCard, prompt)
+        else:
+            over = True
+            check(uCard, cCard, sum_uCard, sum_cCard, prompt)
 
-game()
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    os.system('cls')
+    game()
