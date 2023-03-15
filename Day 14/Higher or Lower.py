9f3c1da31fe599940bc6b943331143434bf05706
Mebz Moren <ellyzajpapas@su.edu.ph>
2023-03-15 10:15:55 +0800

    loop if got the answer correct

--- Day 14/Higher or Lower.py
+++ Day 14/Higher or Lower.py
@@ -3,19 +3,19 @@ import random
 import art
 from game_data import data
 
-info = []
 over = False
 score = 0
 
-def randomize(list):
-    list.append(random.choice(data))
-
-def generate(list):
+def generate():
+    list = []
     while len(list) < 2:
-        randomize(list)
+        list.append(random.choice(data))
+
+    return list
 
 def display(list, index, part):
     print(f"Compare {part}: {list[index]['name']}, a {list[index]['description']}, from {list[index]['country']}")
+    print(f"followers: {list[index]['follower_count']}")
 
 def answer():
     choice = input("Who has more followers? Type 'A' or 'B': ")
@@ -33,20 +33,27 @@ def assignment(choice):
 def check(list, index1, index2, score):
     if list[index1]['follower_count'] > list[index2]['follower_count']:
         score += 1
+        print(art.logo)
         print(f"You're right! Current score: {score}")
         return False
     else:
         print(f"Sorry, that's wrong. Final score: {score}")
         return True
-
+    
+def clear_screen():
+    os.system('cls')
+    
+if not over:
+    print(art.logo)
 
 while not over:
-    print(art.logo)
-    generate(info)
-    display(info, 0, "A")
+    list = generate()
+    display(list, 0, "A")
     print(art.vs)
-    display(info, 1, "B")
+    display(list, 1, "B")
     choice = answer()
     other = assignment(choice)
-    os.system('cls')
-    over = check(info, choice, other, score)
+    clear_screen()
+    over = check(list, choice, other, score)
+    if not over:
+        score += 1
