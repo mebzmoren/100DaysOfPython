a1b0e882f8eb9136e13c58be4de8143e14c0e875
mebzmoren <ellyzajpapas@su.edu.ph>
2023-03-16 15:59:20 +0800

    oop

---
+++ Day 16/Coffee Machine in OOP.py
@@ -0,0 +1,11 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+# item = MenuItem()
+# maker = CoffeeMaker()
+# money_machine = MoneyMachine()
+
+menu.get_items()
+menu.find_drink("latte")
--- Day 16/practice.py
+++ Day 16/practice.py
@@ -1,12 +1,18 @@
-#object oriented programming
-import turtle               #imported module
-timmy = turtle.Turtle()     #fetched a class
-print(timmy)                #prints loction where object is saved
-timmy.shape("turtle")       #changes shape of the object
-timmy.color("red", "green") #changes color
-timmy.forward(100)          #moves turtle forward
-my_screen = turtle.Screen() #window where the turtle will show up
-print(my_screen.canvheight) #object.attribute(data), prints the hight of the canvass
-my_screen.exitonclick()     #allows the program to run until click has been detected
+# #object oriented programming
+# import turtle               #imported module
+# timmy = turtle.Turtle()     #fetched a class
+# print(timmy)                #prints loction where object is saved
+# timmy.shape("turtle")       #changes shape of the object
+# timmy.color("red", "green") #changes color
+# timmy.forward(100)          #moves turtle forward
+# my_screen = turtle.Screen() #window where the turtle will show up
+# print(my_screen.canvheight) #object.attribute(data), prints the hight of the canvass
+# my_screen.exitonclick()     #allows the program to run until click has been detected
 
-import prettytable
+#modifying object attributes & calling methpds
+import prettytable                                                  #import module
+table = prettytable.PrettyTable()                                   #fetched a class
+table.add_column("Pokemon", ["Pikachu", "Squirtle", "Charmander"])  #Creates a column with rows
+table.add_column("Type",["Electric", "Water", "Fire"])
+table.align = "l"                                                   #accesses an attribute/field, aligns the whole table
+print(table)                                                        #prints the table
