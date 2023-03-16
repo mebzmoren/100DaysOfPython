858bd83ff1e472cb985209cd2695e0b546d9f72b
Mebz Moren <ellyzajpapas@su.edu.ph>
2023-03-16 10:56:06 +0800

    depleting resources, adding money, giving change

--- Day 15/Coffee Machine.py
+++ Day 15/Coffee Machine.py
@@ -1,16 +1,3 @@
-#Espresso - 50ml water & 18g coffee
-#Latte - 200ml water, 24g coffee, & 150 ml milk
-#Cappucino - 250ml water, 24g coffee, & 100ml milk
-#Storage - 300ml water, 200ml milk, & 100g coffee
-#Coins - penny (1), nickel (5), dime (10), & quarter (25)
-
-#Requirements:
-#1. Print storage
-#2. Check if enough
-#3. Process coins
-#4. Check transaction successful
-#5. Make coffee
-
 MENU = {
     "espresso": {
         "ingredients": {
@@ -45,28 +32,55 @@ resources = {
 
 done = False
 money = 0
+user_money = 0
 
 def menu_prompt():
-    order = input(f"What would you like? (espresso/latte/cappuucino): ")
+    order = input(f"What would you like? (espresso/latte/cappuccino): ")
     return order
     
 def check_resources(order):
     if order == "espresso":
-        if MENU[order]['ingredients']['water'] <= resources["water"] and MENU[order]['ingredients']['coffee'] <= resources["coffee"]:
+        if MENU[order]['ingredients']['water'] > resources["water"]:
+            print("Sorry not enough water.")
             return False
-    if MENU[order]['ingredients']['water'] <= resources["water"] and MENU[order]['ingredients']['milk'] <= resources["milk"] and MENU[order]['ingredients']['coffee'] <= resources["coffee"]:
-        return False
-    else:
-        return True
+        elif MENU[order]['ingredients']['coffee'] > resources["coffee"]:
+            print("Sorry not enough coffee.")
+            return False
+        else:
+            return True
+    elif order == "latte" or order == "cappuccino":
+        if MENU[order]['ingredients']['water'] > resources["water"]:
+            print("Sorry not enough water.")
+            return False
+        elif MENU[order]['ingredients']['milk'] > resources["milk"]:
+            print("Sorry not enough milk.")
+            return False
+        elif MENU[order]['ingredients']['coffee'] > resources["coffee"]:
+            print("Sorry not enough coffee.")
+            return False
+        else:
+            return True
+        
+def process_money(user_money):
+    quarters = int(input("how many quarters?: "))
+    dimes = int(input("how many dimes?: "))
+    nickels = int(input("how many nickels?: "))
+    pennies = int(input("how many pennies?: "))
+    user_money = (quarters * 0.25) + (dimes * 0.10) + (nickels * 0.05) + (pennies * 0.01)
+    return user_money
     
-def check_money(money, order):
-    if MENU[order]['cost'] < money:
-        return False
-    else:
+def check_money(order, user_money):
+    user_money = process_money(user_money)
+    if MENU[order]['cost'] < user_money:
+        change = user_money - MENU[order]['cost']
+        print(f"Here is ${round(change, 2)} dollars in change.")
         return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+    
 
-    
-def check_prompt(order):
+def check_prompt(order, user_money):
     if order == "report":
         for key in resources:
             if key == "coffee":
@@ -78,14 +92,29 @@ def check_prompt(order):
         return False
     elif order == "off":
         return True
-    elif check_resources(order) == False:
-        if check_money(money, order) == False:
-            #some action
-        return False
-    elif check_resources(order) == True:
-        #some action
-        return True
+    elif order == "espresso" or order == "latte" or order == "cappuccino":
+        enough_resources = check_resources(order)
+        if enough_resources == True:
+            enough_money = check_money(order, user_money)
+            if enough_money == True:
+                return False
+            
+def make_coffee(order, dictionary, available, money):
+    if order == 'espresso':
+        available['water'] -= dictionary[order]['ingredients']['water']
+        available['coffee'] -= dictionary[order]['ingredients']['coffee']
+        money += dictionary[order]['cost']
+        return money
+    elif order == "latte" or order == "cappuccino":
+        available['water'] -= dictionary[order]['ingredients']['water']
+        available['milk'] -= dictionary[order]['ingredients']['milk']
+        available['coffee'] -= dictionary[order]['ingredients']['coffee']
+        money += dictionary[order]['cost']
+        return money
+    print(f"Here is your {order}. Enjoy!")
 
 while not done:
     order = menu_prompt()
-    done = check_prompt(order)
+    done = check_prompt(order, user_money)
+    if done == False:
+        money = make_coffee(order, MENU, resources, money)
