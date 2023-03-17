e29c57942da4f30aba39303e9e05e4efcc23e11e
mebzmoren <ellyzajpapas@su.edu.ph>
2023-03-17 10:29:05 +0800

    making coffee machine oop

--- Day 16/Coffee Machine in OOP.py
+++ Day 16/Coffee Machine in OOP.py
@@ -3,9 +3,30 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 menu = Menu()
-# item = MenuItem()
-# maker = CoffeeMaker()
-# money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+over  = False
 
-menu.get_items()
-menu.find_drink("latte")
+def display_report():
+    coffee_maker.report()
+    money_machine.report()
+
+def prompt(choice):
+    if choice ==  "report":
+        display_report()
+        return False
+    elif choice == "off":
+        return True
+    else:
+        found = menu.find_drink(choice)
+        if found:
+            sufficient = coffee_maker.is_resource_sufficient(found)
+            if sufficient:
+                enough_money = money_machine.make_payment(found.cost)
+                if enough_money:
+                    coffee_maker.make_coffee(found)
+
+while not over:
+    choices = menu.get_items()
+    choice = input(f"What would you like? ({choices}): ")
+    over = prompt(choice)
